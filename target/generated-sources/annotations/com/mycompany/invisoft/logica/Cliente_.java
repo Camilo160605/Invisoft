df818a8350da81c20b8d393af1c93c4df61b3794
Camilo160605 <camilo1665ober@gmail.com>
2023-07-05 12:34:40 -0500

    Commit Invisoft

---
+++ target/generated-sources/annotations/com/mycompany/invisoft/logica/Cliente_.java
@@ -0,0 +1,19 @@
+package com.mycompany.invisoft.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-07-04T23:24:52", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, String> tel_cliente;
+    public static volatile SingularAttribute<Cliente, Integer> id_cliente;
+    public static volatile SingularAttribute<Cliente, String> tipo_Doc_Cliente;
+    public static volatile SingularAttribute<Cliente, String> direc_cliente;
+    public static volatile SingularAttribute<Cliente, String> correo_cliente;
+    public static volatile SingularAttribute<Cliente, String> nom_cliente;
+    public static volatile SingularAttribute<Cliente, String> doc_cliente;
+
+}
