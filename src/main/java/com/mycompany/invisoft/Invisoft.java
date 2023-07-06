9f5c14547b298467cb370964509a213c45909bc4
Camilo160605 <camilo1665ober@gmail.com>
2023-07-06 10:22:27 -0500

    nuevos cambiosen invisoft

--- src/main/java/com/mycompany/invisoft/Invisoft.java
+++ src/main/java/com/mycompany/invisoft/Invisoft.java
@@ -1,13 +1,16 @@
 package com.mycompany.invisoft;
 
 //import com.mycompany.invisoft.igu.Clientes;
-import com.mycompany.invisoft.igu.InicioInvisoft;
+
+import com.mycompany.invisoft.igu.LoginInvisoft;
+
+
 //import com.mycompany.invisoft.igu.Principal;
 
 public class Invisoft {
 
     public static void main(String[] args) {
-        InicioInvisoft empezamosfuerte = new InicioInvisoft();
+        LoginInvisoft empezamosfuerte = new LoginInvisoft();
         empezamosfuerte.setVisible(true);
         empezamosfuerte.setLocationRelativeTo(null);
     }
