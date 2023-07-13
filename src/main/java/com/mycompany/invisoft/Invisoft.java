af21374f347c8f9449b9c431b32bab5472c5d07e
Camilo160605 <camilo1665ober@gmail.com>
2023-07-13 11:59:52 -0500

    11:59

--- src/main/java/com/mycompany/invisoft/Invisoft.java
+++ src/main/java/com/mycompany/invisoft/Invisoft.java
@@ -1,23 +1,16 @@
 package com.mycompany.invisoft;
 
-//import com.mycompany.invisoft.igu.Clientes;
-
+import com.mycompany.invisoft.igu.InicioInvisoft;
 //import com.mycompany.invisoft.igu.LoginInvisoft;
-import com.mycompany.invisoft.igu.Proveedores;
-
-
-//import com.mycompany.invisoft.igu.Principal;
 
 public class Invisoft {
-
     public static void main(String[] args) {
-       /* LoginInvisoft empezamosfuerte = new LoginInvisoft();
-        empezamosfuerte.setVisible(true);
-        empezamosfuerte.setLocationRelativeTo(null);*/
-        
-        Proveedores prov = new Proveedores();
-        prov.setVisible(true);
-        prov.setLocationRelativeTo(null);
-        
+//        LoginInvisoft empezamosfuerte = new LoginInvisoft();
+//        empezamosfuerte.setVisible(true);
+//        empezamosfuerte.setLocationRelativeTo(null);
+
+        InicioInvisoft vista = new InicioInvisoft();
+        vista.setVisible(true);
+        vista.setLocationRelativeTo(null);
     }
 }
