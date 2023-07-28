5a7bf254da9f59a8ee7cd537de304f378a0ab66d
Camilo160605 <camilo1665ober@gmail.com>
2023-07-27 20:49:02 -0500

    hola

--- src/main/java/com/mycompany/invisoft/igu/Ayuda.java
+++ src/main/java/com/mycompany/invisoft/igu/Ayuda.java
@@ -7,6 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
